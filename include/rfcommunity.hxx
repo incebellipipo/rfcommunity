c38591b4e128a2adc0cacea38184c89a6bca9052
Emir Cem Gezer <emircem.gezer@gmail.com>
2017-07-26 15:25:54 +0300

    too many bug fixes

--- include/rfcommunity.hxx
+++ include/rfcommunity.hxx
@@ -31,53 +31,74 @@ class Rfcommunity {
 
     ~Rfcommunity(void);
 
-    bool Connect(const char *dev_addr, const char *remote_addr, int channel);
+    bool Connect(const char *local_addr, const char *remote_addr, int channel);
+    bool Connect();
 
-    bool Bind(const char *dev_addr, const char *remote_addr, int channel);
+    bool Bind(const char *local_addr, const char *remote_addr, int channel);
+    bool Bind();
 
-    bool Release(int dev_id);
+    bool Release(int dev);
     bool Release();
 
+    // This three methods implemented in future
     int Show(const char *dev_name);
-
     int Listen(const char *dev_name, int channel);
-
     int Watch(const char *dev_name, int channel);
 
-    bool Disconnect();
 
-    bool isConnected();
+    void setLocalAddr(const char *local_addr);
+    void setRemoteAddr(const char *remote_addr);
+    void setRemoteAddr(const char *remote_addr, uint8_t channel);
+    void setChannel(int channel);
+
+    int getDev(){return m_dev;}
+    bool isReleased(){return m_is_released;}
+    bool isConnected(){return m_is_connected;}
+    bool isBinded(){return m_is_binded;}
+
+    std::string getRemoteAddr(){return m_remote_addr;}
+    std::string getLocalAddr(){return m_local_addr;}
 
   private:
-    int dev_number_ = 0;
-    char devname_[MAXPATHLEN];
-    int rfcomm_raw_tty_ = 0;
-
-    int s_socket_conn_;
-    int c_socket_conn_;
-    int b_socket_conn_;
-    int fd_socket_conn_;
-
-    int socket_conn_;
-
-
-    int connect_val_;
-    int auth = 0;
-    int encryption = 0;
-    int secure = 0;
-    int master = 0;
-    int linger = 0;
-
-    int ctl_;
-    int file_descriptor_;
-    struct rfcomm_dev_req current_dev_req_;
+    char m_devname[MAXPATHLEN];
+    int  m_rfcomm_raw_tty = 0;
+    int  m_ctl;
+    int  m_file_descriptor;
 
 
   private:
-    bool is_connected_ = false;
-    bool is_released_ = false;
-    bool release_dev_(int dev);
-    bool release_all_(void);
+    bool m_is_connected = false;
+    bool m_is_binded    = false;
+    bool m_is_released  = false;
+
+    uint16_t m_dev = 0;
+    bdaddr_t m_local_bdaddr;
+    bdaddr_t m_remote_bdaddr;
+    struct sockaddr_rc m_local_sockaddr_rc;
+    struct sockaddr_rc m_remote_sockaddr_rc;
+
+    std::string m_local_addr;
+    std::string m_remote_addr;
+
+    int f_create_dev();
+    int f_release_dev();
+    int f_release_dev(int dev);
+    int f_release_all();
+
+    // Connect Method
+    void f_cmd_connect(int ctl, int dev, bdaddr_t *bdaddr, int argc, char **argv);
+    // Listen Method
+    void f_cmd_listen(int ctl, int dev, bdaddr_t *bdaddr, int argc, char **argv);
+    // Watch Method
+    void f_cmd_watch(int ctl, int dev, bdaddr_t *bdaddr, int argc, char **argv);
+    // Bind Method
+    void f_cmd_create(int ctl, int dev, bdaddr_t *bdaddr, int argc, char **argv);
+    // Release Method
+    void f_cmd_release(int ctl, int dev, bdaddr_t *bdaddr, int argc, char **argv);
+    // Show Method
+    void f_cmd_show(int ctl, int dev, bdaddr_t *bdaddr, int argc, char **argv);
+
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -4,18 +4,6 @@
 int main() {
 
   Rfcommunity rfcommunity;
-//  first you need to connect
-//  rfcommunity.Connect(host_device_addr,remote_device_addr);
-//  then release the port
-//  try{
-//    rfcommunity.Release();
-//  } catch (str::runtime_error e){
-//    std::cout << e.what() << std::endl;
-//  }
-//  to disconnect device
-//  rfcommunity.Disconnect();
-
-
 
 
   return 0;
